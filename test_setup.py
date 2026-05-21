326c7f556c0b215b77b9e9763cabda8e0c6dac35
byn2ss <nesrine.tns@gmail.com>
2026-05-22 00:51:17 +0200

    Add files via upload

---
+++ test_setup.py
@@ -0,0 +1,7 @@
+import langchain
+import faiss
+from mistralai.client import MistralClient
+import os
+print("LangChain version:", langchain.__version__)
+print("FAISS est prêt !")
+print( "Environnement Puls-Events opérationnel.")
